8ebde98d7f95ef0d63bc44f7675fcb23bc1f95bf
Andrew Ravn <ravn.andrew@gmail.com>
2020-02-24 19:46:28 -0700

    creating base express app, removing public directory, not needed for basic app

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const express = require('express')
 app.set('view engine', 'pug')
 app.set('views', './views')
 
-app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(require('./controllers'))
